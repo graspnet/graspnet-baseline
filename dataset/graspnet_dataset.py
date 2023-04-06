a6d42dab91044be5bb3c99e601365bd2263625d6
masayuki-kono@mamezou.com <masayuki-kono@gmail.com>
2023-04-06 20:18:02 +0900

    fix import error of container_abcs from torch._six

--- dataset/graspnet_dataset.py
+++ dataset/graspnet_dataset.py
@@ -9,7 +9,7 @@ import scipy.io as scio
 from PIL import Image
 
 import torch
-from torch._six import container_abcs
+from collections import abc as container_abcs
 from torch.utils.data import Dataset
 from tqdm import tqdm
 
